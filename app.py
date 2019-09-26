04f96efc4aac9756c330b515edc3d445139f55ee
AZPL\bjancz <EXT.Bartosz.Jancz@allianz.pl>
2019-09-26 14:37:13 +0200

    gui update

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello bro 1 {name}!</h3>" \
+    html = "<h3>Hello bro 333 {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
